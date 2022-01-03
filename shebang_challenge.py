519a0d288138abe9a38c457681230e5b45c34b07
Manuel <manuel@tlg.com>
2022-01-03 23:04:19 +0000

    manipulating lists

---
+++ shebang_challenge.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+icecream= ["indentation", "spaces"]
+import random
+tlgstudents= ["Aaron","Alex","Alonzo","Brandon","Chris","Francisco","James","Jonathan","Lillian","Manuel","Patrick","Robert","Ryan","Troy","Wes","Henry","Yalined"]
+random_name_array=random.choice(tlgstudents)
+numbafoh= [4]
+icecream.append(numbafoh[0])
+print(icecream)
+#print(f'{tlgstudents[9]} always uses {numbafoh[0]} {icecream[1]} to indent')
+print(f'{random_name_array} always uses {numbafoh[0]} {icecream[1]} to indent')
