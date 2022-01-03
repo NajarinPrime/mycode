0ea8cca137261de8c5b296d47ee57476e2f47804
Manuel <manuel@tlg.com>
2022-01-03 22:06:38 +0000

    learning about input statement

---
+++ challenge2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+name=input("Hello, what is your name?")
+day_of_week=input("Hello " + name + " what is the day of the week?")
+print("Thank you " + name + ", " + day_of_week + " is my favorite day")
